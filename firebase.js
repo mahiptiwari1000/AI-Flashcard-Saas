d71d6594eb019f4d0665677721e5b10f9cec1c2a
unknown <mahiptiwari1000@gmail.com>
2024-08-15 01:09:47 -0500

    Initial Code

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEBbSUYUNs-I5B_9Dnaoq54zEYWXs_yJc",
+  authDomain: "ai-flashcard-saas-7a5b5.firebaseapp.com",
+  projectId: "ai-flashcard-saas-7a5b5",
+  storageBucket: "ai-flashcard-saas-7a5b5.appspot.com",
+  messagingSenderId: "191929505526",
+  appId: "1:191929505526:web:6b7151e1bd93defbcd7ed7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
